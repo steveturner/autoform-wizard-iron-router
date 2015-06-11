7a98fb3cad4846bee0ea33cf4ed87c70e029a16e
Eelco Wiersma <eelco@pagebakers.nl>
2015-06-11 15:42:56 +0700

    Updated package config

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
-  name: 'forwarder:autoform-wizard',
+  name: 'forwarder:autoform-wizard-iron-router',
   summary: 'Flow Router bindings for AutoForm Wizard.',
   version: '0.1.0',
-  git: 'https://github.com/forwarder/meteor-wizard-flow-router.git'
+  git: 'https://github.com/forwarder/meteor-wizard-iron-router.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   
   api.use([
-    'forwarder:autoform-wizard',
-    'meteorhacks:flow-router@0.7.0'
+    'forwarder:autoform-wizard@0.7.0',
+    'iron:router'
   ], 'client');
   
   api.addFiles([
