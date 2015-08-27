c468321d404651c17843e0850f8fdce31bdfe465
Steven Turner <sturner@ctic-inc.com>
2015-08-26 22:05:06 -0600

    adjust package dependencies to use fork

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
-  name: 'forwarder:autoform-wizard-iron-router',
+  name: 'steveturner:autoform-wizard-iron-router',
   summary: 'Iron Router bindings for AutoForm Wizard.',
-  version: '0.1.2',
-  git: 'https://github.com/forwarder/meteor-wizard-iron-router.git'
+  version: '0.1.3',
+  git: 'https://github.com/steveturner/meteor-wizard-iron-router.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   
   api.use([
-    'forwarder:autoform-wizard@0.7.0',
+    'steveturner:autoform-wizard@0.7.1',
     'iron:router@1.0.0'
   ], 'client');
   
